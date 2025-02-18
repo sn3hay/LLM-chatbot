698f2a343cecd24dae6262b128c33833b7997103
sn3hay <snehayad@buffalo.edu>
2025-02-18 14:00:44 -0500

    Trying OpenAI's API

---
+++ home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("LLM Chatbot")
+st.divider()
+st.write("Welcome to the LLM Chatbot! This chatbot is designed to help answer questions about Topic Undecided")
+st.subheader("Trying out something new @sn3hay")
